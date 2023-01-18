2ce9285c203b10b13daf895147b0afc451fa20d5
Dioceus <>
2023-01-18 22:08:01 +0000

    try to setup commands

--- main.py
+++ main.py
@@ -1,27 +1,38 @@
-# This code is based on the following example:
-# https://discordpy.readthedocs.io/en/stable/quickstart.html#a-minimal-bot
-
 import discord
+from discord.ext import commands 
 import os
-from keep_alive import keep_alive()
+from keep_alive import keep_alive
 
-client = discord.Client()
+bot = commands.Bot(command_prefix="!",
+                      case_insensitive = True,
+                      intents=discord.Intents.all())
 
-@client.event
+@bot.event
 async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
+    print('We have logged in as {0.user}'.format(bot))
 
-@client.event
+@bot.event
 async def on_message(message):
-    if message.author == client.user:
+    if message.author == bot.user:
         return
 
     if message.content.startswith('$hello'):
         await message.channel.send('Hello!')
 
+@bot.event
+async def on_command_error(ctx, exception):
+  #When a command fails
+  await ctx.send(f"Error: {exception}", reference = ctx.message)
+  
+@bot.command()
+async def hello(ctx):
+  print("Sending command")
+  await ctx.channel.send("Hello World!")
+
 try:
     keep_alive()
-    client.run(os.getenv("TOKEN"))
+    my_secret = os.environ['DISCORD_BOT_SECRET']
+    bot.run(my_secret)
 except discord.HTTPException as e:
     if e.status == 429:
         print("The Discord servers denied the connection for making too many requests")
