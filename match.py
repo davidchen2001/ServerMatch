23521316072a36be5247602ea0102833803d83c0
David Chen <42048732+davidchen2001@users.noreply.github.com>
2023-03-22 01:23:14 +0000

    add initial implementation for recommender algorithm - need to add multiple matches at once

--- match.py
+++ match.py
@@ -1,3 +1,4 @@
+from config import mongoURI
 import random
 
 DEFAULT = "RANDOM"
@@ -52,4 +53,57 @@ class Match:
     #Consider number of desired matches
     return matches
 
-  
+  def recommenderMatch(self, users):
+
+    #For every pair of users - (2 x number of same keywords)/total_keywords
+
+    matches = {}
+
+    for i in range(len(users)):
+
+      max_coefficient = 0
+      max_coefficient_index = 0
+      introduction = users[i].getIntroduction()
+      userKeywords = self.__parseIntroduction__(introduction)
+
+      for j in range(len(users)):
+
+        otherIntroduction = users[j].getIntroduction()
+        otherKeywords = self.__parseIntroduction__(otherIntroduction)
+
+        for keyword in userKeywords:
+
+          num_same = 0
+          if keyword in otherKeywords:
+            num_same += 1
+
+        total_keywords = len(userKeywords) + len(otherKeywords)
+        dice_coefficient = self.__dice_coefficient__(num_same, total_keywords)
+
+        if max_coefficient < dice_coefficient:
+          max_coefficient = dice_coefficient
+          max_coefficient_index = j 
+
+      firstUser = users[i].getId()
+      secondUser = users[max_coefficient_index].getId()
+      matches[firstUser.getId()] = secondUser.getId()
+      
+    return matches
+          
+  def __dice_coefficient__(self, num_same, num_keywords):
+    return (2 * num_same)/num_keywords
+
+  def __parseIntroduction__(self, introduction):
+    userKeywords = []
+          
+    data = introduction.split('\n')
+
+    for pair in data:
+      label = pair.split(":")[0]
+      keywords = pair.split(":")[1]
+
+    for keyword in keywords:
+      userKeywords.append(keyword.lower())
+
+    return userKeywords
+    
